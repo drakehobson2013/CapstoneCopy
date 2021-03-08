0b8f8bd5032f933976962ccc66a8c3a96859ad7c
Aarika Turner <turneraarika@gmail.com>
2021-03-08 15:10:46 -0500

    push about updates

---
+++ src/components/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const about = () => {
+    return (
+        <div className= 'container'>
+            <h3>About Us</h3>
+            <h4> <em> Meet the Team!</em></h4>
+
+            <div className= 'post-container'>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default about
