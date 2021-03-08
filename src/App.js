c3fa888d9207b31d3804039f3860936962971575
Drake Hobson <drakehobson2013@gmail.com>
2021-03-08 16:23:31 -0500

    imports and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "React"
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import Components
+// import Navbar from "./components/Navbar";
+// import Home  from "./components/Home";
+// import About  from "./components/About";
+// import EatLocal  from "./components/EatLocal";
+// import ShopLocal  from "./components/ShopLocal";
+// import Attractions  from "./components/Attractions";
+// import CincyHistory  from "./components/CincyHistory";
+// import Contact  from "./components/Contact";
 
 function App() {
   return (
+    <Router>
     <div className="App">
+    <Navbar />
+    <br/>
+    {/* <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/eat" component={EatLocal} />
+    <Route path="/shop" component={ShopLocal} />
+    <Route path="/visit" component={Attractions} />
+    <Route path="/learn" component={CincyHistory} />
+    <Route path="/contact" component={Contact} /> */}
       
     </div>
+    </Router>
   );
 }
 
