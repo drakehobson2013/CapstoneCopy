99f3cad30453fb8d041df19c01d57716d98c6e31
Aarika Turner <turneraarika@gmail.com>
2021-03-08 16:08:38 -0500

    pushing About container updates

--- src/components/about.js
+++ src/components/about.js
@@ -12,8 +12,13 @@ const about = () => {
 
             <div className= 'post-container'>
             <img src={Drake} alt="Drake"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quae eius sunt eveniet. Repudiandae, earum veritatis doloremque unde ad dolor!</p>
             <img src={Kiah} alt="Kiah" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis facilis distinctio esse quasi incidunt quisquam explicabo quas maiores quod voluptatibus!</p>
             <img src={Aarika} alt="Aarika" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae soluta magnam nesciunt harum illum quisquam aut suscipit ab maiores.</p>
+            <img src={Ron} alt="Ron" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cumque quia accusantium vitae aliquam perspiciatis itaque vel quae dolore autem.</p>
 
             </div>
             
