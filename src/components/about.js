b2e148ffb708b9c3a16e4805425dabbd182bc9f5
Aarika Turner <turneraarika@gmail.com>
2021-03-09 11:08:09 -0500

    push about updates

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Drake from './Drake.jpg';
 import Kiah from './Kiah.jpg';
-import Aarika from '.Aarika.jpg/'
+import Aarika from './Aarika.jpg';
+import Ron from './Ron'
 
 
 const About = () => {
@@ -11,13 +12,13 @@ const About = () => {
             <h4> <em> Meet the Team!</em></h4>
 
             <div className= 'post-container'>
-            <img src={Drake} alt="Drake"/>
+            <img src={Drake} alt="Drake" id="drake" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quae eius sunt eveniet. Repudiandae, earum veritatis doloremque unde ad dolor!</p>
-            <img src={Kiah} alt="Kiah" />
+            <img src={Kiah} alt="Kiah" id="kiah" />
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis facilis distinctio esse quasi incidunt quisquam explicabo quas maiores quod voluptatibus!</p>
-            <img src={Aarika} alt="Aarika" />
+            <img src={Aarika} alt="Aarika" id="aarika" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae soluta magnam nesciunt harum illum quisquam aut suscipit ab maiores.</p>
-            <img src={Ron} alt="Ron" />
+            <img src={Ron} alt="Ron" id="ron" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cumque quia accusantium vitae aliquam perspiciatis itaque vel quae dolore autem.</p>
 
             </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 
 
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 export default class navbar extends Component {
   render() {
     return (
