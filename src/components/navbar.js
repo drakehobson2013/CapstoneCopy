06d13733259675d0475232c63b2cf786da707046
Ronaldlholt3 <76957909+Ronaldlholt3@users.noreply.github.com>
2021-03-10 11:32:07 -0500

    updated nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">Home</a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Contact</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small" >Contact</a>
  
     <div class="w3-dropdown-hover w3-hide-small">
       <button class="w3-padding-large w3-button" title="More">Todos <i class="fa fa-caret-down"></i></button>     
