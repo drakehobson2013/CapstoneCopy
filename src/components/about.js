46cac8ec513485d63c325023449716fc7ab6b0b5
Aarika Turner <turneraarika@gmail.com>
2021-03-10 16:22:17 -0500

    push pics uploaded to image file for categories

--- src/components/about.js
+++ src/components/about.js
@@ -15,9 +15,9 @@ const About = () => {
             <h4> <em> Meet the Team!</em></h4>
 
             <div className= 'post-container'>
-            <div className= 'card-deck-4'>
+            <div className= 'card-deck-4' id='deck'>
 
-            <div className="card">
+            <div className="card bg-dark">
             <img src={Drake} alt="Drake" id="drake" />
             <p> <blockquote > Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quae eius sunt eveniet. Repudiandae, earum veritatis doloremque unde ad dolor!
             - Drake</blockquote></p>
@@ -29,13 +29,13 @@ const About = () => {
             - Kiah</blockquote></p>
             </div>
 
-            <div className='card'>
+            <div className='card text-white bg-dark'>
             <img src={Aarika} alt="Aarika" id="aarika"/>
             <p> <blockquote > Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae soluta magnam nesciunt harum illum quisquam aut suscipit ab maiores. 
             - Aarika</blockquote></p>
             </div>
 
-            <div className='card'>
+            <div className='card text-dark'>
             <img src={Ron} alt="Ron" id="ron" />
             <p> <blockquote>I just think how amazing it is to me how I knew nothing about code to that now I can actually design wonderful things and let my creativity free through web design.
             - Ron</blockquote> </p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ export default class Navbar extends Component {
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large">Home</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
+    <a href="/about" class="w3-bar-item w3-button w3-padding-large w3-hide-small">About</a>
     <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hide-small" >Contact</a>
  
     <div class="w3-dropdown-hover w3-hide-small">
