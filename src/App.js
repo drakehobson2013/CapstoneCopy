0eaaa9bdfa6c3b66fc9cf8e3ce62dca6ba8cc533
Kiah Pleasant <kiahpleasant@gmail.com>
2021-03-11 15:33:25 -0500

    push to gh

--- src/App.js
+++ src/App.js
@@ -5,17 +5,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home  from "./components/Home";
-import About  from "./components/About";
-<<<<<<< HEAD
-// import EatLocal  from "./components/EatLocal";
 import ShopLocal  from "./components/ShopLocal";
 import Attractions  from "./components/Attractions";
-=======
 import EatLocal  from "./components/EatLocal";
-// import ShopLocal  from "./components/ShopLocal";
-// import Attractions  from "./components/Attractions";
->>>>>>> 2d596ccf9710fad29f768c7241f0d947e0b7db15
-// import CincyHistory  from "./components/CincyHistory";
+import CincyHistory  from "./components/CincyHistory";
 import Contact  from "./components/Contact";
 
 function App() {
@@ -27,29 +20,14 @@ function App() {
     <Navbar />
     <br/>
     <Switch>  
-<<<<<<< HEAD
      <Route path="/about" component={About} />
      <Route path="/contact" component={Contact} />
      <Route path="/attraction" component={Attractions} />  
      <Route path="/shop" component={ShopLocal} />
-    
-    {/*  
-  <Route path="/" component={Home} />
-    <Route path="/restaurant" component={EatLocal} />
-    
-    
-    <Route path="/history" component={CincyHistory} />
-  */ }
-=======
-    <Route path="/about" component={About} />
-    <Route path="/contact" component={Contact} />
-    <Route exact path="/" component={Home} />
+     <Route path="/restaurant" component={EatLocal} />
+     <Route path="/" component={Home} />
+     <Route path="/history" component={CincyHistory} />
  
-    <Route path="/restaurant" component={EatLocal} />
-    {/* <Route path="/shop" component={ShopLocal} /> */}
-    {/* <Route path="/attraction" component={Attractions} /> */}
-    {/* <Route path="/history" component={CincyHistory} /> */}
->>>>>>> 2d596ccf9710fad29f768c7241f0d947e0b7db15
     </Switch> 
      <Footer /> 
     </div>
