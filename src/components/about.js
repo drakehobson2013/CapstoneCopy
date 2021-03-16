8f376ba1c893188ea8f56069c60aeae0d4da3175
Aarika Turner <turneraarika@gmail.com>
2021-03-16 14:34:16 -0400

    cosmetic updates to about section

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ import Ron from './Ron.jpeg';
 import "../index.css";
 import AOS from 'aos'
 import 'aos/dist/aos.css';
-
+import MetroCincy from './MetroCincy.jpg';
 
 
 
@@ -35,11 +35,13 @@ AOS.init();
 <br/>
 <br/>
 <div className="city">
-    <div className="container" data-aos="fade-up">
+    <div className="container aos-init">
     {/* <video id="" preload="auto" autoPlay="true" loop volume="0">
     </video> */}
 
-    <iframe width="875" height="340" preload="auto" autoPlay="true" loop volume="0" src="https://www.youtube.com/embed/G8EUiH00BrU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    {/* <iframe className="vid"  preload="auto" autoPlay="true" loop volume="0" src="https://www.youtube.com/embed/G8EUiH00BrU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+
+    <img className="cincy shadow-lg"  data-aos="fade-down" src={MetroCincy} alt="Metro Cincinnati Aerial Map"/>
     
     </div>
 </div>
@@ -48,39 +50,43 @@ AOS.init();
             <div className= 'post-container'>
             <div className= 'card-deck-4' id='deck'>
 
-            <div className="card aos-init" data-aos='fade-right'>
+            <div className="card aos-init shadow" data-aos='fade-right'>
                 <div className="image">
             <img src={Drake} alt="Drake" id="drake" />
                 </div>
             <p> <blockquote  className="text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quae eius sunt eveniet. Repudiandae, earum veritatis doloremque unde ad dolor!
+            <br/>
             - Drake</blockquote></p>
             </div>
             <br/>
 
-            <div data-aos='fade-left' className='card aos-init' >
+            <div data-aos='fade-left' className='card aos-init shadow' >
                 <div className="image">
              <img src={Kiah} alt="Kiah" id="kiah"/>
                 </div>
             <p > <blockquote  className="text-center" > Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis facilis distinctio esse quasi incidunt quisquam explicabo quas maiores quod voluptatibus!
+            <br/>
             - Kiah</blockquote></p>
             </div>
             <br/>
 
-            <div  className="card aos-init" data-aos='fade-right'>
+            <div  className="card aos-init shadow" data-aos='fade-right'>
                 <div className="image">
             <img src={Aarika} alt="Aarika" id="aarika"/>
                 </div>
             <p> <blockquote  className="text-center"> 
-            Creating this site with my team was a pleasure, and an overall great learning experience. To be able to collaborate and bring together the best of each individuals strong points; creating these amazing pages you see now. Thank you to Kable Academy for giving me the opportunity to learn and promote the foundating skills necessary to build upon, for my present and future career in Web Development.  
+            Creating this site with my team was a pleasure, and an overall great learning experience. To be able to collaborate and bring together the best of each individuals strong points; creating these amazing pages you see now. Thank you to Kable Academy for giving me the opportunity to learn and promote the foundating skills necessary to build upon, for my present and future career in Web Development. 
+            <br/> 
             - Aarika</blockquote></p>
             </div>
             <br/>
 
-            <div data-aos='fade-left' className='card aos-init'>
+            <div data-aos='fade-left' className='card aos-init shadow'>
                 <div className="image">
             <img src={Ron} alt="Ron" id="ron" />
                 </div>
             <p className="text-justify-end"> <blockquote  className="text-center">I just think how amazing it is to me how I knew nothing about code to that now I can actually design wonderful things and let my creativity free through web design.
+            <br/>
             - Ron</blockquote> </p>
             </div>
 
