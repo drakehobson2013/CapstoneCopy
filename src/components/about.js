1c2c271c8e2e0afed2f32c7f56522a720bb40923
Aarika Turner <turneraarika@gmail.com>
2021-03-19 15:39:55 -0400

    updates to about page

--- src/components/about.js
+++ src/components/about.js
@@ -16,23 +16,20 @@ AOS.init();
  
     
         return (
-        <div className= 'container'>
+            <div className="opener">
+        <div className= 'container-fluid'>
             {/* <h3>About the Team</h3> */}
-            <h4 className="text-center"> <em> Meet the Team!</em></h4>
+         <header id="about-header">
+         <div className="container-fluid" id="about-head-bg">
+             <div>
+                 <h3 className="bigger-font text-white">Meet the Team</h3>
+                 <p className="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, eius error. Nostrum corrupti, quam modi repellendus voluptatem tempore reiciendis voluptates explicabo dolorem laborum obcaecati tempora reprehenderit labore dolore quis ad, dolores deleniti fugit, sequi atque! Vel magni harum, nam animi molestiae facere iure iusto fuga beatae corporis qui possimus sit!</p>
+             </div>
+         </div>
 
-<br/>
+         </header>
 
-<div className='about'>
-<div className="container">
-    <p className="about-description">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolorum. Molestias quia voluptatibus quam error, voluptates rerum aut. Accusamus ut est fugiat, id aliquid error magnam saepe tenetur doloribus odio veniam a repellat eligendi esse reprehenderit aperiam placeat ipsa cum. Nobis quidem aut vitae magni sint, beatae pariatur aliquid earum! 
-
-       {/* ^^^^^^^ This will be a short summary on the project, class, and website. ^^^^^ */}
-    </p>
-</div>
-</div>
-
-<br/>
+{/* <br/>
 <br/>
 <div className="city">
     <div className="container aos-init">
@@ -41,10 +38,9 @@ AOS.init();
 
     {/* <iframe className="vid"  preload="auto" autoPlay="true" loop volume="0" src="https://www.youtube.com/embed/G8EUiH00BrU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
 
-    <img className="cincy shadow-lg"  data-aos="fade-down" src={MetroCincy} alt="Metro Cincinnati Aerial Map"/>
+    {/* <img className="cincy shadow-lg"  data-aos="fade-down" src={MetroCincy} alt="Metro Cincinnati Aerial Map"/> */}
     
-    </div>
-</div>
+    
 <br/>
 
             <div className= 'post-container'>
@@ -97,7 +93,7 @@ AOS.init();
         </div>
 
 
-       
+       </div>
     )
 }
 
