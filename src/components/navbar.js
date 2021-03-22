af5b9d9dab913d92f60f3c808b327b02b8f08b3b
Drake Hobson <drakehobson2013@gmail.com>
2021-03-22 11:54:35 -0400

    fixes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,39 +8,33 @@ export default class Navbar extends Component {
   render() {
     return (
 <nav className="navbar navbar-expand-lg top">
-  <a href="/" className="navbar-brand"><img className="FlyingPig" src={FlyingPig} alt=""/></a>
+  <Link to='/' className="navbar-brand"><img className="FlyingPig" src={FlyingPig} alt=""/></Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent">
   <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navBarSupportedContent">
     <ul className="navbar-nav mr-auto">
-      <Link to='/' className="nav-link">Home
-        {/* <a href="/" className="nav-link">Home</a> */}
-      </Link>
-      <Link to='/about' className="nav-link">About
-      {/* <a href="/about" className="nav-link">About</a> */}
-      </Link>
-      <Link to='/contact' className="nav-link">Contact
-      {/* <a href="/contact" className="nav-link">Contact</a> */}
-      </Link>
+      <Link to='/' className="nav-link">Home</Link>
+      <Link to='/about' className="nav-link">About</Link>
+      <Link to='/contact' className="nav-link">Contact</Link>
       <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Things To Do
           </a>
         <div className="dropdown-menu">
         
-        <Link to='/restaurant' className="dropdown-item">Eat Local
-        {/* <a href="/restaurant" className="dropdown-item">Eat local</a> */}
-        </Link>
-        <Link to='/shop' className="dropdown-item">Shop Local
-        {/* <a href="/shop" className="dropdown-item">Shop local</a> */}
-        </Link>
-        <Link to='/attraction' className="dropdown-item">Attractions
-        {/* <a href="/attraction" className="dropdown-item">Attractions</a> */}
-        </Link>
-        <Link to='/history' className="dropdown-item">Cincy History
-        {/* <a href="/history" className="dropdown-item">Cincy History</a> */}
-        </Link>
+        <Link to='/restaurant' className="dropdown-item">Eat Local</Link>
+      
+        
+        <Link to='/shop' className="dropdown-item">Shop Local</Link>
+       
+        
+        <Link to='/attraction' className="dropdown-item">Attractions</Link>
+        
+        
+        <Link to='/history' className="dropdown-item">Cincy History</Link>
+        
+        
         </div>
       </li>
     </ul>
